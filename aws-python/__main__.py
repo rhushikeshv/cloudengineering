08dfb719165d0bb6b938ab32535a85f896dca63b
rhushikeshv <rhushikesh.velankar@hcl.com>
2021-06-26 10:24:10 +0530

    added tags for s3 bucket

--- aws-python/__main__.py
+++ aws-python/__main__.py
@@ -1,3 +1,5 @@
+from typing import Optional, Mapping
+
 import pulumi
 
 from pulumi_aws import s3
@@ -5,8 +7,14 @@ from pulumi_aws import s3
 """An AWS Python Pulumi program"""
 """This is a sample program to create s3 bucket"""
 
+# add a label
+
+
 # Create an AWS resource (S3 Bucket)
-bucket = s3.Bucket('my-bucket')
+bucket = s3.Bucket('my-bucket', tags={
+    "Environment": "Dev",
+    "Name": "My bucket",
+})
 
 # Export the name of the bucket
 pulumi.export('bucket_name', bucket.id)
