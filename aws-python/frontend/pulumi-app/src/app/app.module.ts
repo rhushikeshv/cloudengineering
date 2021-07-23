e8b5686a63adc37b375a8a2977e7a0afa28ef099
rhushikeshv <rhushikesh.velankar@hcl.com>
2021-07-23 10:39:15 +0530

    added drawing, parts and ecr to the dashboard

--- aws-python/frontend/pulumi-app/src/app/app.module.ts
+++ aws-python/frontend/pulumi-app/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PartComponent } from './part/part.component';
+import { DrawingComponent } from './drawing/drawing.component';
+import { EcrComponent } from './ecr/ecr.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PartComponent,
+    DrawingComponent,
+    EcrComponent
   ],
   imports: [
     BrowserModule,
---
+++ aws-python/frontend/pulumi-app/src/app/ecr/ecr.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ecr',
+  templateUrl: './ecr.component.html',
+  styleUrls: ['./ecr.component.css']
+})
+export class EcrComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
